eb5837455a106a4872a066cf7a698308b82c10e5
Boris <bfeen@mail.ru>
2020-04-01 23:49:01 +0300

    added render main tag

--- src/main.js
+++ src/main.js
@@ -51,6 +51,86 @@ const createFiltersTemplate = () => {
     );
 };
 
+const createSortTemplate = () => {
+    return (
+        `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+            <span class="trip-sort__item  trip-sort__item--day"></span>
+
+            <div class="trip-sort__item  trip-sort__item--event">
+              <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event">
+              <label class="trip-sort__btn" for="sort-event">Event</label>
+            </div>
+
+            <div class="trip-sort__item  trip-sort__item--time">
+              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" checked>
+              <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time">
+                Time
+              </label>
+            </div>
+
+            <div class="trip-sort__item  trip-sort__item--price">
+              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+              <label class="trip-sort__btn" for="sort-price">
+                Price
+              </label>
+            </div>
+
+            <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+        </form>`
+    )
+}
+
+const createTripDaysTemplate = () => {
+    return (
+        `<ul class="trip-days">
+            <li class="trip-days__item  day">
+              <div class="day__info"></div>
+
+                <ul class="trip-events__list"></ul>
+            </li>
+        </ul>`
+    );
+};
+
+const createTripPointTemplate = () => {
+    return (
+        `<li class="trip-events__item">
+            <div class="event">
+            <div class="event__type">
+                <img class="event__type-icon" width="42" height="42" src="img/icons/drive.png" alt="Event type icon">
+            </div>
+            <h3 class="event__title">Drive to Chamonix</h3>
+
+            <div class="event__schedule">
+                <p class="event__time">
+                <time class="event__start-time" datetime="2019-03-18T14:30">14:30</time>
+                &mdash;
+                <time class="event__end-time" datetime="2019-03-18T16:05">16:05</time>
+                </p>
+                <p class="event__duration">1H 35M</p>
+            </div>
+
+            <p class="event__price">
+                &euro;&nbsp;<span class="event__price-value">160</span>
+            </p>
+
+            <h4 class="visually-hidden">Offers:</h4>
+            <ul class="event__selected-offers">
+                <li class="event__offer">
+                <span class="event__offer-title">Rent a car</span>
+                &plus;
+                &euro;&nbsp;<span class="event__offer-price">200</span>
+                </li>
+            </ul>
+
+            <button class="event__rollup-btn" type="button">
+                <span class="visually-hidden">Open event</span>
+            </button>
+            </div>
+        </li>`
+    );
+};
+
 const render = (container, template, place='beforeend') => {
     container.insertAdjacentHTML(place, template);
 }
@@ -62,3 +142,15 @@ const tripControls = tripMain.querySelector(`.trip-controls`)
 render(tripMain, createTripInfoTemplate(), 'afterbegin');
 render(tripControls, createMenuTemplate(), 'afterbegin');
 render(tripControls, createFiltersTemplate());
+
+const main = document.querySelector(`.page-main`);
+const tripEvents = main.querySelector(`.trip-events`);
+
+render(tripEvents, createSortTemplate());
+render(tripEvents, createTripDaysTemplate());
+
+const tripEventsList = tripEvents.querySelector(`.trip-events__list`);
+
+for (let i=0; i < POINT_COUNT; i++) {
+    render(tripEventsList, createTripPointTemplate());
+}
