73e92e5f9d76b75eb8b8fbb3ef213ee9d3e90e93
Boris <bfeen@mail.ru>
2020-05-29 21:44:00 +0300

    Final Changes: fixed bugs using ESLint

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,7 +7,7 @@ const formatSimpleNum = (num) => {
     return;
   }
   return String(num).padStart(2,0);
-}
+};
 
 export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
